2fc8cec1c461281ed0120e6a1f828f0a318a2f77
jonny.zhang <jonny.zhang@yunzhihui.com>
2021-12-03 16:17:16 +0800

    fixed: 修复样式

--- web/src/pages/App/ComponentDevelop/index.js
+++ web/src/pages/App/ComponentDevelop/index.js
@@ -3,7 +3,7 @@
  * @Author: zhangzhiyong
  * @Date: 2021-11-09 10:45:26
  * @LastEditors: zhangzhiyong
- * @LastEditTime: 2021-12-03 15:22:51
+ * @LastEditTime: 2021-12-03 16:03:57
  */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState,useEffect, useRef } from "react";
@@ -403,103 +403,6 @@ const ComponentDevelop = observer((props) => {
       </Sider>
       <Content>
           <div className={styles.rightWraper}>
-          {/* <Row className={styles.handleWrap} gutter={10}>
-              <Col span={5}>
-                <span>组件名称：</span>
-                <Input placeholder='请输入'
-                  // style={{width:'calc(100% - 70px)'}}
-                  style={{width:200}}
-                  value={searchName}
-                  onChange={(e)=>{
-                    setSearchName(e.target.value);
-                    // setSearchKey('');
-                    setCurPage(0);
-                    getListData();
-                  }}
-                ></Input>
-              </Col>
-              <Col span={5}>
-                <span>　</span>
-                <Input 
-                  // style={{width:'100%'}} 
-                  style={{ width: 250 }}
-                  placeholder='输入描述/标签/创建人查找组件'
-                  value={searchKey}
-                  onChange={(e)=>{
-                    setSearchKey(e.target.value);
-                    setCurPage(0);
-                    getListData();
-                  }}
-                ></Input>
-              </Col>
-              <Col span={4}>
-                <span>项目名称：</span>
-                <Select
-                  showSearch
-                  allowClear
-                  placeholder='请选择'
-                  // style={{ width: 'calc(100% - 70px)' }}
-                  style={{ width: 150 }}
-                  value={searchProject}
-                  onChange={(val)=>{
-                    setSearchProject(val);
-                    getListData();
-                  }}
-                  filterOption={(input, option) =>
-                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                  }
-                >
-                  {
-                    projectsData.map(item=>{
-                      return <Option value={item.id} key={item.id}>{item.name}</Option>;
-                    })
-                  }
-                </Select>
-              </Col>
-              <Col span={4}>
-                <span>开发状态：</span>
-                <Select
-                  placeholder='请选择'
-                  // style={{ width: 'calc(100% - 70px)' }}
-                  style={{ width: 150 }}
-                  value={searchStatus}
-                  onChange={(val)=>{
-                    setSearchStatus(val);
-                    getListData();
-                  }}
-                >
-                  <Option value='all'>全部</Option>
-                  <Option value="doing">开发中</Option>
-                  <Option value="online">已上线</Option>
-                </Select>
-              </Col>
-              <Col span={4}>
-                <span>组件类别：</span>
-                <Select
-                  placeholder='请选择'
-                  // style={{ width: 'calc(100% - 70px)' }}
-                  style={{ width: 150 }}
-                  value={searchType}
-                  onChange={(val)=>{
-                    setSearchType(val);
-                    getListData();
-                  }}
-                >
-                  <Option value='all'>全部</Option>
-                  <Option value="common">基础组件</Option>
-                  <Option value="project">项目组件</Option>
-                </Select>
-              </Col>
-              <Col span={2}>
-                <Button 
-                  type='primary' 
-                  style={{borderRadius:'3px'}}
-                  onClick={()=>{
-                    setAddModalvisible(true);
-                  }}
-                >添加组件</Button>
-              </Col>
-            </Row> */}
             
             <div className={styles.handleWrap}>
               <div>
